19072fca12e05be823756b7b4c33968acff50a61
Claire Froelich <claire.froelich@gmail.com>
2025-05-24 10:54:54 -0400

    solve day 2, js

---
+++ 02/solution.js
@@ -0,0 +1,42 @@
+const load = require("../util/load");
+
+const raw = load(__dirname + "/input.txt");
+
+/** ------------------------------------- */
+
+// ---- PT 1 -----
+
+// get array of int arrays
+const rows = raw.split("\n").map((r) => r.split(/\s+/).map((i) => parseInt(i)));
+
+function checksumDiff(row) {
+  const s = [...row].sort((a, b) => a - b);
+  return s[s.length - 1] - s[0];
+}
+
+const result = rows.reduce((acc, cur) => acc + checksumDiff(cur), 0);
+console.log("pt1: ", result);
+
+// ---- PT 2 -----
+
+// assume only one evenly divisbile pair per row
+function checksumQuotient(row) {
+  const s = [...row].sort((a, b) => b - a); // sort high -> low
+  let quotient;
+
+  for (let i = 0; i < s.length; i++) {
+    const dividend = s[i];
+    const rest = s.slice(i + 1); // all vals after dividend
+    for (let j = 0; j < rest.length; j++) {
+      const valid = dividend % rest[j] === 0;
+      if (valid) {
+        quotient = dividend / rest[j];
+        break;
+      }
+    }
+  }
+  return quotient;
+}
+
+const result2 = rows.reduce((acc, cur) => acc + checksumQuotient(cur), 0);
+console.log("pt2: ", result2);
