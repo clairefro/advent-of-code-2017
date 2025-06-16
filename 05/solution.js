49922a20be9773dae4ea37c3800a3abc19fb864a
Claire Froelich <claire.froelich@gmail.com>
2025-06-16 09:12:13 -0400

    what

--- 05/solution.js
+++ 05/solution.js
@@ -5,6 +5,7 @@ const raw = load(__dirname + "/input.txt");
 /** ------------------------------------- */
 
 const instructions = raw.split("\n").map((n) => parseInt(n));
+const instructions2 = [...instructions]; // clone to use in pt2
 
 // my stack overflowed when i rawdogged this recursive fn
 function jump(ins, jumps = 0, i = 0) {
@@ -38,16 +39,15 @@ console.log("elapsed: ", t2 - t1, "ms");
 function jump2(ins, jumps = 0, i = 0) {
   if (ins[i] === undefined) return jumps;
   jumps++;
-  const next = i + ins[i];
   ins[i] = ins[i] >= 3 ? ins[i] - 1 : ins[i] + 1;
-
-  return () => jump(ins, jumps, next);
+  const next = i + ins[i];
+  return () => jump2(ins, jumps, next);
 }
 
 console.log("PT2");
 
 const t3 = performance.now();
-console.log(trampoline(jump2(instructions)));
+console.log(trampoline(() => jump2(instructions2)));
 const t4 = performance.now();
 
 console.log("elapsed: ", t4 - t3, "ms");
