d4129a4bef10966a8cc4727105c909b80cd0df15
Claire Froelich <claire.froelich@gmail.com>
2025-06-16 10:52:42 -0400

    pt 2 done

--- 06/solution.js
+++ 06/solution.js
@@ -11,6 +11,8 @@ const raw = load(__dirname + "/input.txt");
 
 // how many redistributions done before  a block sin banks config is produced that has been seen before?
 
+// ----- PT 1 -------
+
 const banks = raw.split(/\s+/).map((b) => +b);
 
 function trampoline(fn) {
@@ -22,7 +24,7 @@ function trampoline(fn) {
 
 function reallocate(_banks, _redists = {}, step = 0) {
   const redist = _banks.join(".");
-  if (redist && !!_redists[redist]) return step;
+  if (redist && _redists[redist] !== undefined) return { step, banks: _banks };
   _redists[redist] = step;
   const nextBanks = redistribute(_banks);
   return () => reallocate(nextBanks, _redists, step + 1);
@@ -30,13 +32,9 @@ function reallocate(_banks, _redists = {}, step = 0) {
 
 function redistribute(_banks) {
   const bs = [..._banks];
-
   const maxV = Math.max(...bs);
-
   const bigI = bs.findIndex((x) => x === maxV);
-
   bs[bigI] = 0; // reset big bank
-
   let i = bigI;
   for (let a = 0; a < maxV; a++) {
     i = (i + 1) % bs.length;
@@ -46,4 +44,11 @@ function redistribute(_banks) {
 }
 
 console.log("PT1");
-console.log(trampoline(() => reallocate(banks)));
+const info = trampoline(() => reallocate(banks));
+console.log(info);
+
+// ----- PT 2 -------
+
+const infoLoop = trampoline(() => reallocate(info.banks));
+console.log("PT2");
+console.log(infoLoop);
